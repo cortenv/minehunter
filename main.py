a48bb7d476921880bcb0d44f73eddc729ce96815
cortenv <vera.corten@hotmail.com>
2023-01-05 08:08:31 +0100

    show numbers

--- main.py
+++ main.py
@@ -1,23 +1,80 @@
 import pygame
 import os
+#import mouse
+from random import random
+##pygame.font.init()
 
 class Board():
-    def __init__(self, size):
+    def __init__(self, size, prob):
         self.size = size
         self.setBoard()
+        self.prob = prob
 
     def setBoard(self):
         self.board = []
         for row in range (self.size[0]):
             row = []
             for col in range(self.size[1]):
-                piece = None
+                hasBomb = random() < prob
+                piece = Piece(hasBomb)
                 row.append(piece)
             self.board.append(row)
+        self.setNeighbors()
+
+    def setNeighbors(self):
+        for row in range(self.size[0]):
+            for col in range(self.size[1]):
+                piece = self.getPiece((row, col))
+                neighbors = self.getListOfNeighbors((row, col))
+                piece.setNeighbors(neighbors)
+
+    def getListOfNeighbors(self, index):
+        neighbors = []
+        for row in range(index[0] - 1, index[0] + 2):
+            for col in range(index[1] - 1, index[1] + 2):
+                outOfBounds = row < 0 or row >= self.size[0] or col < 0 or col >= self.size[1]
+                same = row == index[0] and col == index[1]
+                if (same or outOfBounds):
+                    continue
+                neighbors.append(self.getPiece((row, col)))
+            return neighbors
 
     def getSize(self):
         return self.size
+
+    def getPiece(self, index):
+        return self.board[index [0]][index [1]]
+
+
+class Piece():
+    def __init__(self, hasBomb):
+        self.hasBomb = hasBomb
+        self.clicked = False
+        self.flagged = False
+
+    def getHasBomb(self):
+        return self.hasBomb
+
+    def getClicked(self):
+        return self.clicked
+
+    def getFlagged(self):
+        return self.flagged
+
+    def setNeighbors(self, neighbors):
+        self.neighbors = neighbors
+        self.setNumAround()
+    
+    def setNumAround(self):
+        self.numAround = 0
+        for piece in self.neighbors:
+            if (piece.getHasBomb()):
+                self.numAround += 1
+
+    def getNumAround(self):
+        return self.numAround
             
+
 class Game():
     def __init__(self, board, screenSize):
         self.board = board
@@ -37,6 +94,16 @@ class Game():
             pygame.display.flip()
         pygame.quit()
 
+    def draw(self):
+        topLeft = (0, 0)
+        for row in range(self.board.getSize()[0]):
+            for col in range(self.board.getSize()[1]):
+                piece = self.board.getPiece((row, col))
+                image = self.getImage(piece)
+                self.screen.blit(image, topLeft)
+                topLeft = topLeft[0] + self.pieceSize[0], topLeft[1]
+            topLeft = 0, topLeft[1] + self.pieceSize[1]
+
     def loadImages(self):
         self.images = {}
         for fileName in os.listdir("images"):
@@ -46,18 +113,13 @@ class Game():
             image = pygame.transform.scale(image, self.pieceSize)
             self.images[fileName.split(".")[0]] = image
 
-    def draw(self):
-        topLeft = (0, 0)
-        for row in range(self.board.getSize()[0]):
-            for col in range(self.board.getSize()[1]):
-                image = self.images["tile"]
-                self.screen.blit(image, topLeft)
-                topLeft = topLeft[0] + self.pieceSize[0], topLeft[1]
-            topLeft = 0, topLeft[1] + self.pieceSize[1] 
-        
+    def getImage(self, piece):
+        string = "mine" if piece.getHasBomb() else str(piece.getNumAround())
+        return self.images[string]
 
 size = (9, 9)
-board = Board(size)
+prob = 0.4
+board = Board(size, prob)
 screenSize = (800, 800)
 game = Game(board, screenSize)
 game.run()
